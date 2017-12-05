b7a240dd291e56fab6ab2f29bc4631f3e7c01ef1
CherMet <dddfrdd@gmail.com>
2017-12-05 15:51:27 +0400

    PlaneModelAdding-and-3rdPersonCamera

---
+++ Assets/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	float linearSpeed=0f;
+	float rotationSpeed=40f;
+	public float deltaSpeed = 10f;
+
+	void Start() {
+		Debug.Log ("The game was started now...");
+	}
+
+	void FixedUpdate() {
+		transform.Translate (-Vector3.forward * linearSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.UpArrow))
+			transform.Rotate (-Vector3.right * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.DownArrow))
+			transform.Rotate (-Vector3.left * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.Z))
+			transform.Rotate (Vector3.down * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.X))
+			transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (-Vector3.forward * rotationSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Rotate (-Vector3.back * rotationSpeed * Time.deltaTime);
+		if (Input.GetKeyUp (KeyCode.KeypadPlus))
+			linearSpeed = linearSpeed + deltaSpeed;
+		if (Input.GetKeyUp (KeyCode.KeypadMinus) && linearSpeed>0)
+			linearSpeed = linearSpeed - deltaSpeed;
+	}
+}
---
+++ Assets/surroundSoundSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class surroundSoundSwitch : MonoBehaviour {
+
+	AudioListener surroundSoundSwitcher;
+
+	void Awake () {
+		surroundSoundSwitcher = Camera.main.GetComponent<AudioListener>();
+	}
+
+	void Start () {
+		Debug.Log ("Scene started...");
+	}
+
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.LeftShift))
+			surroundSoundSwitcher.enabled=!surroundSoundSwitcher.enabled;
+	}
+
+}
